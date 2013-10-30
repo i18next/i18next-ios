e01dd4843cee7f0c3661d37b908141f297174d0b
Jean Regisser <jean.regisser@gmail.com>
2013-10-30 11:40:49 +0100

    Added spec for empty value

---
+++ spec/I18NextTranslationEmptyValueSpec.m
@@ -0,0 +1,97 @@
+//
+//  I18NextTranslationEmptyValueSpec.m
+//  i18next
+//
+//  Created by Jean Regisser on 30/10/13.
+//  Copyright (c) 2013 PrePlay, Inc. All rights reserved.
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "Nocilla.h"
+
+#import "I18Next.h"
+
+#import "I18NextSpecHelper.h"
+
+SpecBegin(I18NextTranslationEmptyValue)
+
+describe(@"I18Next", ^{
+    __block I18Next* i18n = nil;
+    
+    beforeAll(^{
+        [[LSNocilla sharedInstance] start];
+    });
+    
+    afterAll(^{
+        [[LSNocilla sharedInstance] stop];
+    });
+    
+    beforeEach(^{
+        i18n = createDefaultI18NextTestInstance();
+    });
+    
+    afterEach(^{
+        [[LSNocilla sharedInstance] clearStubs];
+    });
+    
+    describe(@"translation", ^{
+        
+        describe(@"key with empty string value as valid option", ^{
+            
+            beforeEach(^{
+                i18n.resourcesStore =
+                @{
+                  @"dev": @{ @"translation": @{ @"empty": @"" } },
+                  @"en": @{ @"translation": @{ } },
+                  @"en-US": @{ @"translation": @{ } },
+                  };
+            });
+            
+            it(@"should translate correctly", ^{
+                expect([i18n t:@"empty"]).to.equal(@"");
+            });
+            
+            describe(@"missing on unspecific", ^{
+                
+                beforeEach(^{
+                    i18n.resourcesStore =
+                    @{
+                      @"dev": @{ @"translation": @{ @"empty": @"text" } },
+                      @"en": @{ @"translation": @{ } },
+                      @"en-US": @{ @"translation": @{ @"empty": @"" } },
+                      };
+                    i18n.lang = @"en";
+                });
+                
+                it(@"should translate correctly", ^{
+                    expect([i18n t:@"empty"]).to.equal(@"text");
+                });
+                
+            });
+            
+            describe(@"on specific language", ^{
+                
+                beforeEach(^{
+                    i18n.resourcesStore =
+                    @{
+                      @"dev": @{ @"translation": @{ @"empty": @"text" } },
+                      @"en": @{ @"translation": @{ } },
+                      @"en-US": @{ @"translation": @{ @"empty": @"" } },
+                      };
+                });
+                
+                it(@"should translate correctly", ^{
+                    expect([i18n t:@"empty"]).to.equal(@"");
+                });
+                
+            });
+            
+        });
+        
+    });
+    
+});
+
+SpecEnd
