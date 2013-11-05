9b55d97d78becb7c8ca7a435543c3d3149138417
Jean Regisser <jean.regisser@gmail.com>
2013-11-05 08:02:45 +0100

    Fixed sprintf spec float format

--- spec/I18NextTranslationSprintfSpec.m
+++ spec/I18NextTranslationSprintfSpec.m
@@ -46,7 +46,7 @@ describe(@"I18Next translation", ^{
               @"en-US": @{ @"translation": @{
                                    @"interpolationTest1": @"The first 4 letters of the english alphabet are: %s, %s, %s and %s",
                                    @"interpolationTest2": @"Hello %(users[0].name)s, %(users[1].name)s and %(users[2].name)s",
-                                   @"interpolationTest3": @"These mixed strings and decimals: %@, %f, %1.2f"
+                                   @"interpolationTest3": @"These are mixed strings and decimals: %@, %f, %.2f"
                                    } },
               };
         });
@@ -54,15 +54,15 @@ describe(@"I18Next translation", ^{
         it(@"should replace passed in key/values", ^{
             NSString* value = [i18n t:@"interpolationTest1" options:@{ @"sprintf": I18NEXT_SPRINTF_ARGS(@"a".UTF8String, @"b".UTF8String, @"c".UTF8String, @"d".UTF8String) }];
             expect(value).to.equal(@"The first 4 letters of the english alphabet are: a, b, c and d");
-            NSString* value2 = [i18n t:@"interpolationTest3" options:@{ @"sprintf": I18NEXT_SPRINTF_ARGS(@"a", 1.2f, 1.2f) }];
-            expect(value2).to.equal(@"These mixed strings and decimals: a, 1.200000, 1.20");
+            NSString* value2 = [i18n t:@"interpolationTest3" options:@{ @"sprintf": I18NEXT_SPRINTF_ARGS(@"a", 6.3f, 7.8f) }];
+            expect(value2).to.equal(@"These are mixed strings and decimals: a, 6.300000, 7.80");
         });
 
         it(@"should recognize the sprintf syntax and automatically add the sprintf processor", ^{
             NSString* value = [i18n t:@"interpolationTest1", @"a".UTF8String, @"b".UTF8String, @"c".UTF8String, @"d".UTF8String];
             expect(value).to.equal(@"The first 4 letters of the english alphabet are: a, b, c and d");
-            NSString* value2 = [i18n t:@"interpolationTest3", @"a", 1.2f, 1.2f];
-            expect(value2).to.equal(@"These mixed strings and decimals: a, 1.200000, 1.20");
+            NSString* value2 = [i18n t:@"interpolationTest3", @"a", 6.3f, 7.8f];
+            expect(value2).to.equal(@"These are mixed strings and decimals: a, 6.300000, 7.80");
         });
         
     });
