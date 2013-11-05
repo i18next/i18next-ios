86cd91899f481906d7fdff372011964e866f171f
Jean Regisser <jean.regisser@gmail.com>
2013-11-05 08:02:23 +0100

    Updated sprint spec to test both %s and %@ usage

--- spec/I18NextTranslationSprintfSpec.m
+++ spec/I18NextTranslationSprintfSpec.m
@@ -44,7 +44,7 @@ describe(@"I18Next translation", ^{
               @"dev": @{ @"translation": @{ } },
               @"en": @{ @"translation": @{ } },
               @"en-US": @{ @"translation": @{
-                                   @"interpolationTest1": @"The first 4 letters of the english alphabet are: %@, %@, %@ and %@",
+                                   @"interpolationTest1": @"The first 4 letters of the english alphabet are: %s, %s, %s and %s",
                                    @"interpolationTest2": @"Hello %(users[0].name)s, %(users[1].name)s and %(users[2].name)s",
                                    @"interpolationTest3": @"These mixed strings and decimals: %@, %f, %1.2f"
                                    } },
@@ -52,14 +52,14 @@ describe(@"I18Next translation", ^{
         });
         
         it(@"should replace passed in key/values", ^{
-            NSString* value = [i18n t:@"interpolationTest1" options:@{ @"sprintf": I18NEXT_SPRINTF_ARGS(@"a", @"b", @"c", @"d") }];
+            NSString* value = [i18n t:@"interpolationTest1" options:@{ @"sprintf": I18NEXT_SPRINTF_ARGS(@"a".UTF8String, @"b".UTF8String, @"c".UTF8String, @"d".UTF8String) }];
             expect(value).to.equal(@"The first 4 letters of the english alphabet are: a, b, c and d");
             NSString* value2 = [i18n t:@"interpolationTest3" options:@{ @"sprintf": I18NEXT_SPRINTF_ARGS(@"a", 1.2f, 1.2f) }];
             expect(value2).to.equal(@"These mixed strings and decimals: a, 1.200000, 1.20");
         });
 
         it(@"should recognize the sprintf syntax and automatically add the sprintf processor", ^{
-            NSString* value = [i18n t:@"interpolationTest1", @"a", @"b", @"c", @"d"];
+            NSString* value = [i18n t:@"interpolationTest1", @"a".UTF8String, @"b".UTF8String, @"c".UTF8String, @"d".UTF8String];
             expect(value).to.equal(@"The first 4 letters of the english alphabet are: a, b, c and d");
             NSString* value2 = [i18n t:@"interpolationTest3", @"a", 1.2f, 1.2f];
             expect(value2).to.equal(@"These mixed strings and decimals: a, 1.200000, 1.20");
