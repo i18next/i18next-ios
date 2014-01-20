83176d192e535b0cfcc5eaedb9768c82a6b02cd6
Jean Regisser <jean.regisser@gmail.com>
2014-01-20 14:34:40 +0100

    Added load behavior spec
    - make sure langLoadType works correctly
    - make sure fallback lang can be disabled

---
+++ spec/I18NextLoadBehaviorSpec.m
@@ -0,0 +1,116 @@
+//
+//  I18NextLoadBehaviorSpec.m
+//  i18next
+//
+//  Created by Jean Regisser on 20/01/14.
+//  Copyright (c) 2014 PrePlay, Inc. All rights reserved.
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "Nocilla.h"
+
+#import "I18Next.h"
+
+#import "I18NextSpecHelper.h"
+
+SpecBegin(I18NextLoadBehavior)
+
+describe(@"I18Next", ^{
+    __block I18Next* i18n = nil;
+    __block I18NextOptions* options = nil;
+    
+    beforeEach(^{
+        i18n = createDefaultI18NextTestInstance();
+        options = [I18NextOptions optionsFromDict:i18n.options];
+    });
+    
+    describe(@"setting load", ^{
+        
+        describe(@"to current", ^{
+            
+            beforeEach(^AsyncBlock {
+                stubRequest(@"GET", @"http:/example.com/locales/en-US/translation.json")
+                .andReturn(200)
+                .withBody(fixtureData(@"locales/en-US/translation.json"));
+                stubRequest(@"GET", @"http:/example.com/locales/en/translation.json")
+                .andReturn(200)
+                .withBody(fixtureData(@"locales/en/translation.json"));
+                stubRequest(@"GET", @"http:/example.com/locales/dev/translation.json")
+                .andReturn(200)
+                .withBody(fixtureData(@"locales/dev/translation.json"));
+                
+                options.langLoadType = I18NextLangLoadTypeCurrent;
+                options.resourcesBaseURL = [NSURL URLWithString:@"http://example.com"];
+                [i18n loadWithOptions:options.asDictionary completion:^(NSError *error) {
+                    done();
+                }];
+            });
+            
+            it(@"should provide loaded resources for translation", ^{
+                expect([i18n t:@"simple_en-US"]).to.equal(@"ok_from_en-US");
+                expect([i18n t:@"simple_en"]).toNot.equal(@"ok_from_en");
+                expect([i18n t:@"simple_dev"]).to.equal(@"ok_from_dev");
+            });
+            
+        });
+        
+        describe(@"to unspecific", ^{
+            
+            beforeEach(^AsyncBlock {
+                stubRequest(@"GET", @"http:/example.com/locales/en-US/translation.json")
+                .andReturn(200)
+                .withBody(fixtureData(@"locales/en-US/translation.json"));
+                stubRequest(@"GET", @"http:/example.com/locales/en/translation.json")
+                .andReturn(200)
+                .withBody(fixtureData(@"locales/en/translation.json"));
+                stubRequest(@"GET", @"http:/example.com/locales/dev/translation.json")
+                .andReturn(200)
+                .withBody(fixtureData(@"locales/dev/translation.json"));
+                
+                options.langLoadType = I18NextLangLoadTypeUnspecific;
+                options.resourcesBaseURL = [NSURL URLWithString:@"http://example.com"];
+                [i18n loadWithOptions:options.asDictionary completion:^(NSError *error) {
+                    done();
+                }];
+            });
+            
+            it(@"should provide loaded resources for translation", ^{
+                expect([i18n t:@"simple_en-US"]).toNot.equal(@"ok_from_en-US");
+                expect([i18n t:@"simple_en"]).to.equal(@"ok_from_en");
+                expect([i18n t:@"simple_dev"]).to.equal(@"ok_from_dev");
+            });
+            
+        });
+        
+    });
+    
+    describe(@"with fallback language set to nil", ^{
+        
+        beforeEach(^AsyncBlock {
+            stubRequest(@"GET", @"http:/example.com/locales/en-US/translation.json")
+            .andReturn(200)
+            .withBody(fixtureData(@"locales/en-US/translation.json"));
+            stubRequest(@"GET", @"http:/example.com/locales/en/translation.json")
+            .andReturn(200)
+            .withBody(fixtureData(@"locales/en/translation.json"));
+            
+            options.fallbackLang = nil;
+            options.resourcesBaseURL = [NSURL URLWithString:@"http://example.com"];
+            [i18n loadWithOptions:options.asDictionary completion:^(NSError *error) {
+                done();
+            }];
+        });
+        
+        it(@"should provide loaded resources for translation", ^{
+            expect([i18n t:@"simple_en-US"]).to.equal(@"ok_from_en-US");
+            expect([i18n t:@"simple_en"]).to.equal(@"ok_from_en");
+            expect([i18n t:@"simple_dev"]).toNot.equal(@"ok_from_dev");
+        });
+        
+    });
+    
+});
+
+SpecEnd
